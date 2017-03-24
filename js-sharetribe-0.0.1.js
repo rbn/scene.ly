730ad873e8e2de160263f3694b87ee407edcef22
rbn <richnyman@gmail.com>
2017-03-24 16:11:14 -0400

    www
    www

--- js-sharetribe-0.0.1.js
+++ js-sharetribe-0.0.1.js
@@ -1,9 +1,9 @@
+
+  
+  
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <div class="g-hangout-container" style="display:none">
-<div class="g-hangout" data-render="createhangout"></div><script
-  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
-  crossorigin="anonymous" ></script>
+<div class="g-hangout" data-render="createhangout"></div>
  </div>
 
 <script>
