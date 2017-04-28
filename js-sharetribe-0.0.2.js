8795fc2079d896a68db44b53d8ecd4517a819c48
rbn <richnyman@gmail.com>
2017-04-27 21:01:56 -0400

    updates
    notes

--- js-sharetribe-0.0.2.js
+++ js-sharetribe-0.0.2.js
@@ -1,3 +1,16 @@
+<style>
+.marketplace-lander .coverimage-fade  { background:rgba(62, 40, 173, 0.4) !important; }
+
+.marketplace-lander .marketplace-title-header h1 { color: #fff !important; }
+
+div#host-disclaimer { background:rgba(62, 40, 173, 0.4) !important; padding: 18px; color:white; }
+
+.home-fluid-thumbnail-grid-author {display:none;}
+
+div.quantity-description { display: none; }
+
+.price-container > label:after { content: " per 30 minutes"; }
+</style>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <div class="g-hangout-container" style="display:none">
 <div class="g-hangout" data-render="createhangout"></div><script
@@ -20,11 +33,31 @@ jQuery(function() {
   
    // -------------------------------------
    
+   // -------- listing page --------------
    
+   $('div.listing-author').closest('div.row-with-divider').hide();
+   // -----------------------------------
    
    // ------------ landing page - logged in -------------
    
-   $('div.wrapper').prepend('<div><h1>Hire peers and mentors to help you rehearse for your next job interview, big presentation, or any time the spotlight is on you.</h1></div>');
+   var $cover_image = $('<div class="coverimage">' + 
+    '<figure class="marketplace-cover-small fluidratio"></figure>' + 
+    '<div class="coverimage-fade">' + 
+    '<figure class="marketplace-cover-small-fade fluidratio"></figure>' + 
+    '</div>' + 
+    '</div>');
+   
+   var $div_marketplace = $('<div class="title-container">' + 
+      '<div class="title-header-wrapper">' +
+      '<div class="marketplace-title-header">' +
+      '<h1>Find a scene coach to practice your mock interview</h1>' +
+      '</div>' +
+      '</div>' +
+      '</div>');
+      
+   if ( ! $('section.marketplace-lander').children().length ){   
+    $('section.marketplace-lander').prepend($div_marketplace).prepend($cover_image);
+   }
    
    
    // -------------------------------------
@@ -35,7 +68,7 @@ jQuery(function() {
    
   // get transaction id - this is a SaaS hack - need to change
   var tid = $('a.confirm').attr('href')
-  tid = tid.split('/')[4];
+  tid = tid ? tid.split('/')[4] : "";
 
   // get listing url
   var listing_url = $('div.text-part:contains("fulfill the order")').find('a').last().attr('href');
@@ -72,16 +105,34 @@ jQuery(function() {
   // get calendly handle
   
   $.get( listing_url , function( data ) {
-    var calendly_handle = $(data).find('b:contains("Calendly")').closest('div').text(); //.split(':')[1];    
-    console.log(calendly_handle);
+    var calendly_handle = $(data).find('b:contains("Calendly")').closest('div').text(); //.split(':')[1];   
+    calendly_handle = calendly_handle ? calendly_handle.split(':')[1] : "";
     $calendly_link.attr('href', 'https://calendly.com/' + calendly_handle);
-});
-
+  });
 
   //--------------------------------
+  
+  
+  // ---- new listing form ---
+  $('div#new_listing_form').prepend('<div id="host-disclaimer"><h1 style="color:white !important">A note about becoming a scene coach</h1>' + 
+    '<ul><li>you must have a calendly account for scheduling appointments</li>' + 
+    '<li>you must have a linked in account</li>' + 
+    '<li>you must agree to the terms and conditions of becoming a host.  please review here(make link)</li></ul></div>');
+  
+  // -------------------------
 
 
-
-
+  // ---- swap out hour for 30 mins -----
+  $('article.page-content span').text(function (i, old) {
+     return old
+         .replace('hour', '30 mins');
+  });
+  
+  $('div.quantity-description').text(function (i, old) {
+     return old
+         .replace('hour', '30 mins');
+  });
+  // -----------------------------------
+  
 });
 </script>
